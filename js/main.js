6199597842257a3f3cdd645a848c7205953ee3c6
andreacookson1 <andrea.cookson1@gmail.com>
2017-04-05 19:26:47 -0700

    updates to project

--- js/main.js
+++ js/main.js
@@ -32,35 +32,35 @@ $('main-nav').slideToggle(300);
 
 var stickySidebar = $('.sticky');
 
-if (stickySidebar.length > 0) { 
-  var stickyHeight = stickySidebar.height(),
-      sidebarTop = stickySidebar.offset().top;
-}
+// if (stickySidebar.length > 0) { 
+//   var stickyHeight = stickySidebar.height(),
+//       sidebarTop = stickySidebar.offset().top;
+// }
 
-on scroll move the sidebar
-$(window).scroll(function () {
-  if (stickySidebar.length > 0) {   
-    var scrollTop = $(window).scrollTop();
+// on scroll move the sidebar
+// $(window).scroll(function () {
+//   if (stickySidebar.length > 0) {   
+//     var scrollTop = $(window).scrollTop();
             
-    if (sidebarTop < scrollTop) {
-      stickySidebar.css('top', scrollTop - sidebarTop);
+//     if (sidebarTop < scrollTop) {
+//       stickySidebar.css('top', scrollTop - sidebarTop);
 
-      // stop the sticky sidebar at the footer to avoid overlapping
-      var sidebarBottom = stickySidebar.offset().top + stickyHeight,
-          stickyStop = $('.content-area').offset().top + $('.content-area').height();
-      if (stickyStop < sidebarBottom) {
-        var stopPosition = $('.content-area').height() - stickyHeight;
-        stickySidebar.css('top', stopPosition);
-      }
-    }
-    else {
-      stickySidebar.css('top', '0');
-    } 
-  }
-});
+//       // stop the sticky sidebar at the footer to avoid overlapping
+//       var sidebarBottom = stickySidebar.offset().top + stickyHeight,
+//           stickyStop = $('.content-area').offset().top + $('.content-area').height();
+//       if (stickyStop < sidebarBottom) {
+//         var stopPosition = $('.content-area').height() - stickyHeight;
+//         stickySidebar.css('top', stopPosition);
+//       }
+//     }
+//     else {
+//       stickySidebar.css('top', '0');
+//     } 
+//   }
+// });
 
-$(window).resize(function () {
-  if (stickySidebar.length > 0) {   
-    stickyHeight = stickySidebar.height();
-  }
-});
+// $(window).resize(function () {
+//   if (stickySidebar.length > 0) {   
+//     stickyHeight = stickySidebar.height();
+//   }
+// });
